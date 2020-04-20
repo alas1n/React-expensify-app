6a1da3c1f11f07c4b4ed2e66b119f834c1f6da68
ali_m <you@example.com>
2020-04-20 18:44:32 +0430

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -3,16 +3,28 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import getVisibleExpenses from './selectors/expenses'
-import {addExpenses} from './actions/expenses'
+// import getVisibleExpenses from './selectors/expenses'
+// import {addExpenses} from './actions/expenses'
 import 'react-dates/lib/css/_datepicker.css'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
+// import moment from 'moment'
 
 
 
 const store = configureStore()
 
+console.log('test')
+// store.subscribe(() => {
+//     const state = store.getState();
+//     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+//     console.log(visibleExpenses)
+// })
+
+
+// store.dispatch(addExpenses({ description: 'Rent', createdAt: moment().subtract('1','day'), amount: 200, note: 'shiiiit not again' }))
+// store.dispatch(addExpenses({ description: 'water bill', createdAt: moment().subtract('3','day'), amount: 3400, note: 'Must be share' }))
+// store.dispatch(addExpenses({ description: 'Gas bill', createdAt: moment().subtract('2','day'), amount: 5100, note: 'should be more' }))
 
 
 
